2bd7667de119da0709ded279f886d4e2c4b39f99
Valen1611 <vschneider@fi.uba.ar>
2025-03-17 14:52:41 -0300

    directo desde script

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -4,10 +4,12 @@ server_address="server"
 server_port="12345"
 msg="Hello World"
 
-response = $(echo msg | nc $server_address $server_port);
-
-if [ "$response" == "$msg" ]; then
+docker run --rm --network tp0_testing_net alpine:latest /bin/sh -c '
+  apk add --no-cache netcat-openbsd && \
+  response=\$(echo $msg | nc $server_address $server_port) && \
+    if [ "$response" == "$msg" ]; then
     echo "action: test_echo_server | result: success"
-else
+  else
     echo "action: test_echo_server | result: fail"
-fi
+  fi
+'
